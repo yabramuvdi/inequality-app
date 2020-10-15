5a9a0960456ec069ec945dd60d8c1d0257bd0e46
yabramuvdi <yabran.muvdi@gmail.com>
2020-10-15 23:48:00 +0200

    deployment update

--- main.sh
+++ main.sh
@@ -1,9 +1,9 @@
 sudo apt update
-sudo apt install python3-pip tmux htop gunicorn
+sudo apt install python3-pip tmux htop gunicorn python3-dev nginx
 
 python3 -m pip install -U pip setuptools wheel
 python3 -m pip install -r requirements.txt
-pip3 install gunicorn
+pip3 install gunicorn flask
 
 gunicorn app:server -b :8000
 #python3 app.py
